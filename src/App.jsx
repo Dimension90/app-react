7ac7e9baffeebeb03a0522d8c9dce8eb0d0e8753
Dimension90 <163392062+Dimension90@users.noreply.github.com>
2024-06-13 11:39:07 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
-import MainFooter from "./components/MainFooter/MainFooter";
 
 import styles from "./App.module.css";
 
