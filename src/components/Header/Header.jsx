4ae89d5e09fb96649c4aa7a82bbdb3cb5d04ea31
Dimension90 <163392062+Dimension90@users.noreply.github.com>
2024-06-13 15:07:41 +0700

    burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,38 @@
+import React, { useState } from "react";
 import Link from "../Link/Link";
+import { slide as Menu } from "react-burger-menu";
+
 import styles from "./Header.module.css";
 
 import union from "../../assets/img/Union.svg";
 import search from "../../assets/img/24px.svg";
 import favorites from "../../assets/img/Union (Stroke) (Stroke).svg";
+import burger from "../../assets/img/burger.svg";
 import background from "../../assets/img/background.svg";
 
 function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
+      <Link to="/">
         <div className={styles.logoBackground}></div>
-        <Link to="/">
+        <div className={styles.logo}>
           все_ <br />
           тарифы
-        </Link>
-      </div>
+        </div>
+      </Link>
       <div className={styles.location}>
         <img src={union} alt="Локация" className={styles.locationLogo} />
         <Link to="/" className={styles.city}>
           кемерово
         </Link>
       </div>
+
       <div className={styles.menu}>
         <div className={styles.searchWrapper}>
           <img src={search} alt="Поиск" className={styles.searchLogo} />
@@ -52,6 +63,33 @@ function Header() {
           </Link>
         </div>
       </div>
+      <Menu
+        isOpen={isMenuOpen}
+        onClose={toggleMenu}
+        customBurgerIcon={<img src={burger} alt="Burger Menu" />}
+      >
+        <Link to="/" onClick={toggleMenu}>
+          провайдеры
+        </Link>
+        <Link to="/" onClick={toggleMenu}>
+          рейтинг
+        </Link>
+        <Link to="/" onClick={toggleMenu}>
+          тарифы
+        </Link>
+        <Link to="/" onClick={toggleMenu}>
+          акции
+        </Link>
+        <Link to="/" onClick={toggleMenu}>
+          избранное
+        </Link>
+        <Link to="/" onClick={toggleMenu}>
+          +7 (800) 425-19-99
+        </Link>
+        <Link to="/" onClick={toggleMenu} className={styles.callLink}>
+          перезвоните мне
+        </Link>
+      </Menu>
     </div>
   );
 }
